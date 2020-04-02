4279c9d7d1498caa7f6063c9beeb2bf374296d9b
Liam <61776869+ChaosLM99@users.noreply.github.com>
2020-04-03 10:25:24 +1100

    Task A-C

--- game.py
+++ game.py
@@ -5,3 +5,22 @@ import time
 import csv
 
 sense = SenseHat()
+
+class WriteCSV():
+    def __init__(self, filename, winscore):
+        # Declare Variables
+        self.__winscore = winscore
+
+    # If it's the first time, create a file with the write option.
+    def firstTime(self):
+        with open("winner.csv", "w", newline="") as csvfile:
+            self.__writer = csv.writer(csvfile)
+            self.__writer.writerow(["Winner Score"])
+            csvfile.close()
+
+    # Write contents to the CSV file with the append option, to not overwrite.
+    def write(self):
+        with open("winner.csv", "a", newline="") as csvfile:
+            self.__writer = csv.writer(csvfile)
+            self.__writer.writerow([str(self.__winscore)])
+            csvfile.close()
