b2de8f7ec6ab6249978df7d7628e0c2e44b7507f
Liam <61776869+ChaosLM99@users.noreply.github.com>
2020-04-03 11:20:46 +1100

    Tasks A-C

--- game.py
+++ game.py
@@ -6,21 +6,28 @@ import csv
 
 sense = SenseHat()
 
+sense.show_message("Welcome. Shake pi. First to 30 wins. Good luck!")
+sense.clear()
+
+class rollDie:
+    def __init__(self, roll):
+        self.roll = electronicDie.roll
+
 class WriteCSV():
     def __init__(self, filename, winscore):
         # Declare Variables
-        self.__winscore = winscore
+        self.winscore = winscore
 
     # If it's the first time, create a file with the write option.
     def firstTime(self):
         with open("winner.csv", "w", newline="") as csvfile:
-            self.__writer = csv.writer(csvfile)
-            self.__writer.writerow(["Winner Score"])
+            self.writer = csv.writer(csvfile)
+            self.writer.writerow(["Winner Score"])
             csvfile.close()
 
     # Write contents to the CSV file with the append option, to not overwrite.
     def write(self):
         with open("winner.csv", "a", newline="") as csvfile:
-            self.__writer = csv.writer(csvfile)
-            self.__writer.writerow([str(self.__winscore)])
+            self.writer = csv.writer(csvfile)
+            self.writer.writerow([str(self.winscore)])
             csvfile.close()
