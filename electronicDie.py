77d1192f4e73e15012d8d36de2a0f02b627acd40
Hesh <49469036+splishie@users.noreply.github.com>
2020-04-04 19:26:05 +1100

    Update electronicDie.py

--- electronicDie.py
+++ electronicDie.py
@@ -2,115 +2,113 @@ from sense_hat import SenseHat
 import random
 import time
 import sys
-import game
 
 
 sense = SenseHat()
 sense.clear()
 
-y = (255, 255, 0) #Yellow
-b = (0, 0, 0) #Black
+y = (255, 255, 0)  # Yellow
+b = (0, 0, 0)  # Black
 
 one = [
-b,b,b,b,b,b,b,b,
-b,b,b,b,b,b,b,b,
-b,b,b,b,b,b,b,b,
-b,b,b,y,y,b,b,b,
-b,b,b,y,y,b,b,b,
-b,b,b,b,b,b,b,b,
-b,b,b,b,b,b,b,b,
-b,b,b,b,b,b,b,b
+    b, b, b, b, b, b, b, b,
+    b, b, b, b, b, b, b, b,
+    b, b, b, b, b, b, b, b,
+    b, b, b, y, y, b, b, b,
+    b, b, b, y, y, b, b, b,
+    b, b, b, b, b, b, b, b,
+    b, b, b, b, b, b, b, b,
+    b, b, b, b, b, b, b, b
 ]
 
 two = [
-b,b,b,b,b,b,b,b,
-b,y,y,b,b,b,b,b,
-b,y,y,b,b,b,b,b,
-b,b,b,b,b,b,b,b,
-b,b,b,b,b,b,b,b,
-b,b,b,b,b,y,y,b,
-b,b,b,b,b,y,y,b,
-b,b,b,b,b,b,b,b
+    b, b, b, b, b, b, b, b,
+    b, y, y, b, b, b, b, b,
+    b, y, y, b, b, b, b, b,
+    b, b, b, b, b, b, b, b,
+    b, b, b, b, b, b, b, b,
+    b, b, b, b, b, y, y, b,
+    b, b, b, b, b, y, y, b,
+    b, b, b, b, b, b, b, b
 ]
 
 three = [
-b,b,b,b,b,b,b,b,
-b,y,y,b,b,b,b,b,
-b,y,y,b,b,b,b,b,
-b,b,b,y,y,b,b,b,
-b,b,b,y,y,b,b,b,
-b,b,b,b,b,y,y,b,
-b,b,b,b,b,y,y,b,
-b,b,b,b,b,b,b,b
+    b, b, b, b, b, b, b, b,
+    b, y, y, b, b, b, b, b,
+    b, y, y, b, b, b, b, b,
+    b, b, b, y, y, b, b, b,
+    b, b, b, y, y, b, b, b,
+    b, b, b, b, b, y, y, b,
+    b, b, b, b, b, y, y, b,
+    b, b, b, b, b, b, b, b
 ]
 
 four = [
-b,b,b,b,b,b,b,b,
-b,y,y,b,b,y,y,b,
-b,y,y,b,b,y,y,b,
-b,b,b,b,b,b,b,b,
-b,b,b,b,b,b,b,b,
-b,y,y,b,b,y,y,b,
-b,y,y,b,b,y,y,b,
-b,b,b,b,b,b,b,b
+    b, b, b, b, b, b, b, b,
+    b, y, y, b, b, y, y, b,
+    b, y, y, b, b, y, y, b,
+    b, b, b, b, b, b, b, b,
+    b, b, b, b, b, b, b, b,
+    b, y, y, b, b, y, y, b,
+    b, y, y, b, b, y, y, b,
+    b, b, b, b, b, b, b, b
 ]
 
 five = [
-b,y,y,b,b,y,y,b,
-b,y,y,b,b,y,y,b,
-b,b,b,b,b,b,b,b,
-b,b,b,y,y,b,b,b,
-b,b,b,y,y,b,b,b,
-b,b,b,b,b,b,b,b,
-b,y,y,b,b,y,y,b,
-b,y,y,b,b,y,y,b
+    b, y, y, b, b, y, y, b,
+    b, y, y, b, b, y, y, b,
+    b, b, b, b, b, b, b, b,
+    b, b, b, y, y, b, b, b,
+    b, b, b, y, y, b, b, b,
+    b, b, b, b, b, b, b, b,
+    b, y, y, b, b, y, y, b,
+    b, y, y, b, b, y, y, b
 ]
 
 six = [
-b,y,y,b,b,y,y,b,
-b,y,y,b,b,y,y,b,
-b,b,b,b,b,b,b,b,
-b,y,y,b,b,y,y,b,
-b,y,y,b,b,y,y,b,
-b,b,b,b,b,b,b,b,
-b,y,y,b,b,y,y,b,
-b,y,y,b,b,y,y,b
+    b, y, y, b, b, y, y, b,
+    b, y, y, b, b, y, y, b,
+    b, b, b, b, b, b, b, b,
+    b, y, y, b, b, y, y, b,
+    b, y, y, b, b, y, y, b,
+    b, b, b, b, b, b, b, b,
+    b, y, y, b, b, y, y, b,
+    b, y, y, b, b, y, y, b
 ]
 
 
-
 def roll():
-    r = random.randint(1,6)
+    r = random.randint(1, 6)
     if r == 1:
-        sense.show_pixels(one)
+        sense.set_pixels(one)
     elif r == 2:
-        sense.show_pixels(two)
+        sense.set_pixels(two)
     elif r == 3:
-        sense.show_pixels(three)
+        sense.set_pixels(three)
     elif r == 4:
-        sense.show_pixels(four)
+        sense.set_pixels(four)
     elif r == 5:
-        sense.show_pixels(five)
+        sense.set_pixels(five)
     elif r == 6:
-        sense.show_pixels(six)
+        sense.set_pixels(six)
+    return r
 
-while True:
-    try:
-        acceleration = sense.get_accelerometer_raw()
-        x = acceleration['x']
-        y = acceleration['y']
-        z = acceleration['z']
 
-        x = abs(x)
-        y = abs(y)
-        z = abs(z)
+if __name__ == "__main__":
+    while True:
+        try:
+            x, y, z = sense.get_accelerometer_raw().values()
 
-        if x > 1.5 or y > 1.5 or z > 1.5:
-            roll()
-            time.sleep(2)
+            x = abs(x)
+            y = abs(y)
+            z = abs(z)
+
+            if x > 1.5 or y > 1.5 or z > 1.5:
+                roll()
+                time.sleep(2)
+                sense.clear()
+                time.sleep(1)
+
+        except KeyboardInterrupt:
             sense.clear()
-            time.sleep(1)
-            
-    except KeyboardInterrupt:
-        sense.clear()
-        sys.exit()
+            sys.exit()
