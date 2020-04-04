b0b2e27aac532fa1d3b504fafefff60f4b5d63a7
Liam <61776869+ChaosLM99@users.noreply.github.com>
2020-04-04 22:15:59 +1100

    CSV now complete and prints

--- game.py
+++ game.py
@@ -10,9 +10,29 @@ sense = SenseHat()
 # sense.show_message("Welcome. Shake pi. First to 30 wins. Good luck!")
 sense.clear()
 
+# WriteCSV class used to import the results
+# Into a CSV file, so it can be read
+# By the user.
 
-# rollDie class used to call
-# The electronicDie file and the roll method.
+class WriteCSV():
+    def __init__(self, p1score, p2score):
+        # Declare Variables
+        self._p1score = p1score
+        self._p2score = p2score
+
+    # If it's the first time, create a file with the write option.
+    def firstTime(self):
+        with open("winner.csv", "w", newline="") as csvfile:
+            self.writer = csv.writer(csvfile)
+            self.writer.writerow(["Player 1 Score", "Player 2 Score"])
+            csvfile.close()
+
+    # Write contents to the CSV file with the append option, to not overwrite.
+    def write(self):
+        with open("winner.csv", "a", newline="") as csvfile:
+            self.writer = csv.writer(csvfile)
+            self.writer.writerow([str(self._p1score), str(self._p2score)])
+            csvfile.close()
 
 class game:
     def playerRoll(self, player, score):
@@ -41,49 +61,44 @@ class game:
         return cumulativeScore
 
     def playGame(self):
-        p1score = 0
-        p2score = 0
+        self.p1score = 0
+        self.p2score = 0
         player1 = "P1"
         player2 = "P2"
 
-        while p1score < 30 and p2score < 30:
-            p1score = self.playerRoll(player1, p1score)
-            if p1score >= 30:
+        # Writing the CSV variables for creating
+        # And appending the report.
+        self._p1score = self.p1score
+        self._p2score = self.p2score
+        self.__writeCSV = None
+        self.__firstTime = True
+
+        while self.p1score < 30 and self.p2score < 30:
+            self.p1score = self.playerRoll(player1, self.p1score)
+            if self.p1score >= 30:
                 sense.show_message("Player 1 wins!")
                 sense.clear()
+                
+                # Write contents to winner.CSV
+                self.__writeCSV = WriteCSV(self._p1score, self._p2score)
+                if (self.__firstTime):
+                    self.__writeCSV.firstTime()
+                    self.__firstTime = False
+                self.__writeCSV.write()
                 sys.exit()
             else:
-                p2score = self.playerRoll(player2, p2score)
-                if p2score >= 30:
+                self.p2score = self.playerRoll(player2, self.p2score)
+                if self.p2score >= 30:
                     sense.show_message("Player 2 wins!")
                     sense.clear()
+
+                    # Write contents to winner.csv
+                    self.__writeCSV = WriteCSV(self._p1score, self.p2score)
+                    if (self.__firstTime):
+                        self.__writeCSV.firstTime()
+                        self.__firstTime = False
+                    self.__writeCSV.write()
                     sys.exit()
-
-
-# WriteCSV class used to import the results
-# Into a CSV file, so it can be read
-# By the user.
-
-
-class WriteCSV():
-    def __init__(self, winscore):
-        # Declare Variables
-        self.winscore = winscore
-
-    # If it's the first time, create a file with the write option.
-    def firstTime(self):
-        with open("winner.csv", "w", newline="") as csvfile:
-            self.writer = csv.writer(csvfile)
-            self.writer.writerow(["Winner Score"])
-            csvfile.close()
-
-    # Write contents to the CSV file with the append option, to not overwrite.
-    def write(self):
-        with open("winner.csv", "a", newline="") as csvfile:
-            self.writer = csv.writer(csvfile)
-            self.writer.writerow([str(self.winscore)])
-            csvfile.close()
-
 
 # The main class is used to control the program,
 # It will initialize necessary variables and
@@ -93,16 +108,5 @@ class main():
         run_game = game()
         run_game.playGame()
 
-        # Write CSV variables
-        self.__writeCSV = None
-        self.__firstTime = True
-
-        # Write contents to CSV
-        self.__writeCSV = WriteCSV(self.winscore)
-        if (self.__firstTime):
-            self.__writeCSV.firstTime()
-            self.__firstTime = False
-        self.__writeCSV.write()
-
-
+# Call the main class, for the program to be executed.
 main()
